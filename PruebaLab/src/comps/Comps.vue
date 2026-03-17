353cf00b8ffd65fba04943804ffc506ba3b15d59
DAOZ2004 <orellanadaniel461@gmail.com>
2026-03-16 22:02:27 -0600

    cambios

---
+++ PruebaLab/src/comps/Comps.vue
@@ -0,0 +1,45 @@
+<script>
+   export default{
+    data(){
+        return{
+            contador:0,
+            nombre:"kjvcklvccvcv"
+        }
+    },
+
+    methods:{
+        saludar(){
+            alert("Alerta")
+        },
+        incrementar(){
+            this.contador++
+        },
+        escribir(event){
+            this.nombre = event.target.value
+        }
+    }
+
+
+
+
+   }
+
+</script>
+
+
+<template>
+    <button @click="saludar">Presiona aqui</button>
+    <button v-on:click="saludar">Click aqui</button>
+
+   <p>{{ contador }}</p>
+   <button @click="incrementar">Aumentar</button>
+
+   <input @input="Escribir"  type="text" placeholder="Escribe">
+   <p>Hola {{ nombre }}, como te encuentras</p>
+
+</template>
+
+
+<style>
+
+</style>
